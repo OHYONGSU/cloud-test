4265adb6f1a0082c19e3c88b22d32c9803fe5dc1
Jaime <lcalmsky@gmail.com>
2023-09-20 00:43:21 +0900

    feat: 설정 값 불러오기 테스트

--- src/main/java/com/example/cloudtest/CloudTestApplication.java
+++ src/main/java/com/example/cloudtest/CloudTestApplication.java
@@ -1,32 +1,47 @@
 package com.example.cloudtest;
 
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class CloudTestApplication {
 
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-  @Value("${redis-url}")
-  private String redisUrl;
-
-  @Value("${database-url}")
-  private String databaseUrl;
   public static void main(String[] args) {
     SpringApplication.run(CloudTestApplication.class, args);
   }
 
+  @Component
+  @Slf4j
+  @RequiredArgsConstructor
+  static class TestRunner implements ApplicationRunner {
 
-  @PostConstruct
-  void setUp () {
+    private final Environment environment;
 
-    logger.info("redis url : " + redisUrl);
-    logger.info("database url : " + databaseUrl);
+    @Value("${host.api.venom}")
+    private String venom;
 
+    @Value("${szs.sdk.venom.api.key}")
+    private String key;
+
+    @Override
+    public void run(ApplicationArguments args) {
+      log.info("print config start");
+      log.info("{}", environment);
+      log.info("{}", venom);
+      log.info("{}", key);
+      log.info("print config end");
+    }
   }
 }
